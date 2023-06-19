682c9767d8a6b51c0fc5c41b30652c08f52103ee
Steve <Steve@student-net-vpn-0082.intern.ethz.ch>
2023-06-20 04:15:32 +0800

    change dir

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ parser.add_argument(
             ' (requires ground truth pose and intrinsics)')
 
 parser.add_argument(
-    '--detector', choices={'superpoint', 'sift'}, default='superpoint',
+    '--detector', choices={'superpoint', 'sift'}, default='sift',
     help='Keypoint detector')
 parser.add_argument(
     '--superglue', choices={'indoor', 'outdoor'}, default='indoor',
@@ -128,7 +128,7 @@ parser.add_argument(
     '--batch_size', type=int, default=1,
     help='batch_size')
 parser.add_argument(
-    '--train_path', type=str, default='/Users/Steve/Desktop/SP/training_set/experiment/dataset/70/images', # MSCOCO2014_yingxin
+    '--train_path', type=str, default='/home/gao/dataset/70/images'#'/Users/Steve/Desktop/SP/training_set/experiment/dataset/70/images', # MSCOCO2014_yingxin
     help='Path to the directory of training imgs.')
 # parser.add_argument(
 #     '--nfeatures', type=int, default=1024,
